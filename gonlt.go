f55a9b7bca9b703ece5b854bf0f22f90697c59d5
douglaszuqueto <douglas.zuqueto@gmail.com>
2022-12-09 11:25:42 -0300

    Add support for auto login

--- gonlt.go
+++ gonlt.go
@@ -1,7 +1,9 @@
 package gonlt
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/douglaszuqueto/gonlt/credentials"
@@ -13,6 +15,9 @@ const (
 )
 
 type Client struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
 	creds credentials.Credentials
 	rest  krest.Client
 
@@ -31,7 +36,12 @@ func NewClient(creds credentials.Credentials) (*Client, error) {
 
 	rest := krest.New(10 * time.Second)
 
-	return &Client{
+	ctx, cancel := context.WithCancel(context.Background())
+
+	client := &Client{
+		ctx:    ctx,
+		cancel: cancel,
+
 		creds: creds,
 		rest:  rest,
 
@@ -40,7 +50,56 @@ func NewClient(creds credentials.Credentials) (*Client, error) {
 		Connection: NewConnectionService(rest, &creds),
 		Device:     NewDeviceService(rest, &creds),
 		Message:    NewMessageService(rest, &creds),
-	}, nil
+	}
+
+	if err := client.autoLogin(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// autoLogin will try to login if the credentials has the AutoLogin flag set to true
+func (c *Client) autoLogin() error {
+	if !c.creds.AutoLogin {
+		return nil
+	}
+
+	_, err := c.Auth.Login(context.Background(), c.creds.Email, c.creds.Passwd)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Minute)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-c.ctx.Done():
+				log.Println("gonlt: client stopping")
+				return
+			case <-ticker.C:
+				log.Println("gonlt: trying to login")
+
+				_, err := c.Auth.Login(context.Background(), c.creds.Email, c.creds.Passwd)
+				if err != nil {
+					log.Println(err)
+				}
+
+				log.Println("gonlt: token refreshed")
+			}
+		}
+	}()
+
+	return nil
+}
+
+// Stop will stop the client
+func (c *Client) Stop() {
+	c.cancel()
+
+	log.Println("gonlt: client stopped")
 }
 
 // build endpoint
